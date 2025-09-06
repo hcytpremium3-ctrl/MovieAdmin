50ca4f9754f5e0d31a4f8618022ff18659b1d0ac
Hemantchouhan <Hemant.chouhan123@gmail.com>
2025-09-06 13:50:39 +0530

    Added vercel

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'http://localhost:3000/api'
+// const API_BASE_URL = 'http://localhost:3000/api'
+const API_BASE_URL = 'https://serene-kelpie-d1e0aa.netlify.app/api'
 
 export async function apiRequest(endpoint, options = {}, apiKey) {
   const response = await fetch(`${API_BASE_URL}${endpoint}`, {
